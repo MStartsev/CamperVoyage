f186670d4cdde0d27c03bd112b1ed66c1caca4d3
M. A. Startsev <m.a.startsev@gmail.com>
2024-04-17 04:04:51 +0300

    added homepage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,18 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navigation from 'components/Navigation/Navigation';
+import css from './header.module.css';
 
 const Header = () => {
   return (
     <>
-      <header>
+      <header className={css.header}>
         <Navigation />
       </header>
       <main>
-        <div>
-          <Suspense fallback={<p>Loading...</p>}>
-            <Outlet />
-          </Suspense>
-        </div>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Outlet />
+        </Suspense>
       </main>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,25 @@
+import { Link } from 'react-router-dom';
+import css from './pages.module.css';
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={css.wrapper}>
+      <section className={css.homepage}>
+        <div className={css.container}>
+          <div className={css.text_wrapper}>
+            <h1 className={css.title}>Rent a camper!</h1>
+            <p className={css.homepage_text}>
+              Ready to escape the ordinary and embrace the extraordinary? Rent a
+              camper today and set forth on a journey filled with endless
+              possibilities and boundless adventures. Your next great outdoor
+              escapade awaits!
+            </p>
+            <Link to="/catalog" className={css.button_link}>
+              Book now
+            </Link>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
